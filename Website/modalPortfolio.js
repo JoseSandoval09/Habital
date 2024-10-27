7d28a9c648118e32249a7c5f5712ef9fd3a12993
JoseSandoval09 <joseeesp.09@gmail.com>
2024-10-27 13:29:05 -0600

    Added modal info for all projects

--- Website/modalPortfolio.js
+++ Website/modalPortfolio.js
@@ -1,13 +1,236 @@
 const proyectos = {
     1: {
-        imagenes: ['/Website/Resources/Portfolio/1.DEPARTAMENTOSCAMPOS/7-10DPTOSCAMPOS.webp','/Website/Resources/Portfolio/1.DEPARTAMENTOSCAMPOS/2-10DPTOSCAMPOS.webp'],
+        imagenes: [
+            '/Website/Resources/Portfolio/1DEPARTAMENTOSCAMPOS/1-10DPTOSCAMPOS.webp',
+            '/Website/Resources/Portfolio/1DEPARTAMENTOSCAMPOS/2-10DPTOSCAMPOS.webp',
+            '/Website/Resources/Portfolio/1DEPARTAMENTOSCAMPOS/3-10DPTOSCAMPOS.webp',
+            '/Website/Resources/Portfolio/1DEPARTAMENTOSCAMPOS/4-10DPTOSCAMPOS.webp',
+            '/Website/Resources/Portfolio/1DEPARTAMENTOSCAMPOS/5-10DPTOSCAMPOS.webp',
+            '/Website/Resources/Portfolio/1DEPARTAMENTOSCAMPOS/6-10DPTOSCAMPOS.webp',
+            '/Website/Resources/Portfolio/1DEPARTAMENTOSCAMPOS/7-10DPTOSCAMPOS.webp',
+            '/Website/Resources/Portfolio/1DEPARTAMENTOSCAMPOS/8-10DPTOS CAMPOS.webp',
+            '/Website/Resources/Portfolio/1DEPARTAMENTOSCAMPOS/9-10DPTOS CAMPOS.webp',
+            '/Website/Resources/Portfolio/1DEPARTAMENTOSCAMPOS/10DPTOS CAMPOS.webp'
+        ],
         cliente: 'Particular',
         anio: '2023 - Presente',
         ubicacion: 'Tijuana, Baja California',
         superficie: '554.00 m²',
-        descripcion: 'Diseño y construcción de departamentos'
+        descripcion: 'Diseño y construcción de departamentos.'
+    },
+    2: {
+        imagenes: [
+            '/Website/Resources/Portfolio/2DEPARTAMENTOSCASCADA/1-3DPTOSCASCADA.webp',
+            '/Website/Resources/Portfolio/2DEPARTAMENTOSCASCADA/2-3DPTOSCASCADA.webp',
+            '/Website/Resources/Portfolio/2DEPARTAMENTOSCASCADA/3-3DPTOSCASCADA.webp'
+        ],
+        cliente: 'Particular',
+        anio: '2024',
+        ubicacion: 'Rosarito, Baja California',
+        superficie: '704.00 m²',
+        descripcion: 'Diseño arquitectónico, diseño y cálculo de las instalaciones: hidráulica, sanitaria y pluvial de seis departamentos distribuidos en tres niveles, un nivel de sótano de estacionamiento y una terraza.'
+    },
+    3: {
+        imagenes: [
+            '/Website/Resources/Portfolio/3OFICINASADMAC/1-5OFICINASADMAC.webp',
+            '/Website/Resources/Portfolio/3OFICINASADMAC/2-5OFICINASADMAC.webp',
+            '/Website/Resources/Portfolio/3OFICINASADMAC/3-5OFICINASADMAC.webp',
+            '/Website/Resources/Portfolio/3OFICINASADMAC/4-5OFICINASADMAC.webp',
+            '/Website/Resources/Portfolio/3OFICINASADMAC/5-5OFICINASADMAC.webp'
+        ],
+        cliente: 'ADMAC',
+        anio: '2023',
+        ubicacion: 'Tijuana, Baja California',
+        superficie: '88.00 m²',
+        descripcion: 'Diseño de oficinas y mobiliario.'
+    },
+    4: {
+        imagenes: [
+            '/Website/Resources/Portfolio/4CASAHABITACIONSO/1-7CASAHABSO.webp',
+            '/Website/Resources/Portfolio/4CASAHABITACIONSO/2-7CASAHABSO.webp',
+            '/Website/Resources/Portfolio/4CASAHABITACIONSO/3-7CASAHABSO.webp',
+            '/Website/Resources/Portfolio/4CASAHABITACIONSO/4-7CASAHABSO.webp',
+            '/Website/Resources/Portfolio/4CASAHABITACIONSO/5-7CASAHABSO.webp',
+            '/Website/Resources/Portfolio/4CASAHABITACIONSO/6-7CASAHABSO.webp',
+            '/Website/Resources/Portfolio/4CASAHABITACIONSO/7-7CASAHABSO.webp'],
+        cliente: 'Particular',
+        anio: '2024',
+        ubicacion: 'Rosarito, Baja California',
+        superficie: '340.00 m²',
+        descripcion: 'Diseño de casa habitación.'
+    },
+    5: {
+        imagenes: [
+            '/Website/Resources/Portfolio/5CASAHABITACIONTS/1-3CASAHABTS.webp',
+            '/Website/Resources/Portfolio/5CASAHABITACIONTS/2-3CASAHABTS.webp',
+            '/Website/Resources/Portfolio/5CASAHABITACIONTS/3-3CASAHABTS.webp'],
+        cliente: 'Particular',
+        anio: '2024',
+        ubicacion: 'Amado Nervo, Nayarit',
+        superficie: '229.75 m²',
+        descripcion: 'Diseño de casa habitación.'
+    },
+    6: {
+        imagenes: [
+            '/Website/Resources/Portfolio/6CASAHABITACIONAG/1-7CASAHABAG.webp',
+            '/Website/Resources/Portfolio/6CASAHABITACIONAG/2-7CASAHABAG.webp',
+            '/Website/Resources/Portfolio/6CASAHABITACIONAG/3-7CASAHABAG.webp',
+            '/Website/Resources/Portfolio/6CASAHABITACIONAG/4-7CASAHABAG.webp',
+            '/Website/Resources/Portfolio/6CASAHABITACIONAG/5-7CASAHABAG.webp',
+            '/Website/Resources/Portfolio/6CASAHABITACIONAG/6-7CASAHABAG.webp',
+            '/Website/Resources/Portfolio/6CASAHABITACIONAG/7-7CASAHABAG.webp'],
+        cliente: 'Particular',
+        anio: '2023',
+        ubicacion: 'Nochistlán de Mejía, Zacatecas',
+        superficie: '371.25 m²',
+        descripcion: 'Diseño de casa habitación y terraza.'
+    },
+    7: {
+        imagenes: [
+            '/Website/Resources/Portfolio/7CASAHABITACIONMS/1-3CASAHABMS.webp',
+            '/Website/Resources/Portfolio/7CASAHABITACIONMS/2-3CASAHABMS.webp',
+            '/Website/Resources/Portfolio/7CASAHABITACIONMS/3-3CASAHABMS.webp'],
+        cliente: 'Particular',
+        anio: '2023',
+        ubicacion: 'Nochistlán de Mejía, Zacatecas',
+        superficie: '348.50 m²',
+        descripcion: 'Diseño de casa habitación.'
+    },
+    8: {
+        imagenes: [
+            '/Website/Resources/Portfolio/8CENTROCOMUNITARIOPRADERAS/1-3CENTROCOMP.webp',
+            '/Website/Resources/Portfolio/8CENTROCOMUNITARIOPRADERAS/2-3CENTROCOMP.webp',
+            '/Website/Resources/Portfolio/8CENTROCOMUNITARIOPRADERAS/3-3CENTROCOMP.webp'],
+        cliente: 'Centro Comunitario Praderas',
+        anio: '2024',
+        ubicacion: 'Tijuana, Baja California',
+        superficie: '199.00 m²',
+        descripcion: 'Diseño de área social para eventos.'
+    },
+    9: {
+        imagenes: [
+            '/Website/Resources/Portfolio/9BODEGASCOMERCIALESZJ/1-6BODEGASCOMZJ.webp',
+            '/Website/Resources/Portfolio/9BODEGASCOMERCIALESZJ/2-6BODEGASCOMZJ.webp',
+            '/Website/Resources/Portfolio/9BODEGASCOMERCIALESZJ/3-6BODEGASCOMZJ.webp',
+            '/Website/Resources/Portfolio/9BODEGASCOMERCIALESZJ/4-6BODEGASCOMZJ.webp',
+            '/Website/Resources/Portfolio/9BODEGASCOMERCIALESZJ/5-6BODEGASCOMZJ.webp',
+            '/Website/Resources/Portfolio/9BODEGASCOMERCIALESZJ/6-6BODEGASCOMZJ.webp'],
+        cliente: 'Particular',
+        anio: '2024',
+        ubicacion: 'Villa Hidalgo, Jalisco',
+        superficie: '1575.20 m²',
+        descripcion: 'Diseño de bodegas comerciales.'
+    },
+    10: {
+        imagenes: [
+            '/Website/Resources/Portfolio/10FERREMATERIALESJIMENEZ/1-5FERREMATERIALESJ.webp',
+            '/Website/Resources/Portfolio/10FERREMATERIALESJIMENEZ/2-5FERREMATERIALESJ.webp',
+            '/Website/Resources/Portfolio/10FERREMATERIALESJIMENEZ/3-5FERREMATERIALESJ.webp',
+            '/Website/Resources/Portfolio/10FERREMATERIALESJIMENEZ/4-5FERREMATERIALESJ.webp',
+            '/Website/Resources/Portfolio/10FERREMATERIALESJIMENEZ/5-5FERREMATERIALESJ.webp'],
+        cliente: 'Particular',
+        anio: '2023',
+        ubicacion: 'Nochistlán de Mejía, Zacatecas',
+        superficie: '883.30 m²',
+        descripcion: 'Diseño de ferretería y bodega de materiales.'
+    },
+    11: {
+        imagenes: [
+            '/Website/Resources/Portfolio/11REMODELACIONCASAHABITACIONJMT/1-5REMCASAHABJMT.webp',
+            '/Website/Resources/Portfolio/11REMODELACIONCASAHABITACIONJMT/2-5REMCASAHABJMT.webp',
+            '/Website/Resources/Portfolio/11REMODELACIONCASAHABITACIONJMT/3-5REMCASAHABJMT.webp',
+            '/Website/Resources/Portfolio/11REMODELACIONCASAHABITACIONJMT/4-5REMCASAHABJMT.webp',
+            '/Website/Resources/Portfolio/11REMODELACIONCASAHABITACIONJMT/5-5REMCASAHABJMT.webp'],
+        cliente: 'Particular',
+        anio: '2024',
+        ubicacion: 'Guadalupe, Zacatecas',
+        superficie: '68.00 m²',
+        descripcion: 'Remodelación de fachada, diseño de sala de lectura y área social.'
+    },
+    12: {
+        imagenes: [
+            '/Website/Resources/Portfolio/12REMODELACIONCASAHABITACIONPS/1-6REMCASAHABPS.webp',
+            '/Website/Resources/Portfolio/12REMODELACIONCASAHABITACIONPS/2-6REMCASAHABPS.webp',
+            '/Website/Resources/Portfolio/12REMODELACIONCASAHABITACIONPS/3-6REMCASAHABPS.webp',
+            '/Website/Resources/Portfolio/12REMODELACIONCASAHABITACIONPS/4-6REMCASAHAB.webp',
+            '/Website/Resources/Portfolio/12REMODELACIONCASAHABITACIONPS/5-6REMCASAHAB.webp',
+            '/Website/Resources/Portfolio/12REMODELACIONCASAHABITACIONPS/6-6REMCASAHAB.webp'],
+        cliente: 'Particular',
+        anio: '2024',
+        ubicacion: 'Aguascalientes, Aguascalientes',
+        superficie: '139.60 m²',
+        descripcion: 'Remodelación de fachada y diseño de interior de casa habitación.'
+    },
+    13: {
+        imagenes: [
+            '/Website/Resources/Portfolio/13REMODELACIONFACHADAEH/1-2REMFACHADAEH.webp',
+            '/Website/Resources/Portfolio/13REMODELACIONFACHADAEH/2-2REMFACHADAEH.webp'],
+        cliente: 'Particular',
+        anio: '2024',
+        ubicacion: 'Tijuana, Baja California',
+        superficie: 'N/A',
+        descripcion: 'Remodelación de fachada de casa habitación.'
+    },
+    14: {
+        imagenes: [
+            '/Website/Resources/Portfolio/14REMODELACIONFACHADAAR/1-2REMFACHADAAR.webp',
+            '/Website/Resources/Portfolio/14REMODELACIONFACHADAAR/2-2REMFACHADAAR.webp'],
+        cliente: 'Particular',
+        anio: '2024',
+        ubicacion: 'Tijuana, Baja California',
+        superficie: 'N/A',
+        descripcion: 'Remodelación de fachada de casa habitación.'
+    },
+    15: {
+        imagenes: [
+            '/Website/Resources/Portfolio/15DEPARTAMENTOSHENRY/1-6DPTOSHENRY.webp',
+            '/Website/Resources/Portfolio/15DEPARTAMENTOSHENRY/2-6DPTOSHENRY.webp',
+            '/Website/Resources/Portfolio/15DEPARTAMENTOSHENRY/3-6DPTOSHENRY.webp',
+            '/Website/Resources/Portfolio/15DEPARTAMENTOSHENRY/4-6DPTOSHENRY.webp',
+            '/Website/Resources/Portfolio/15DEPARTAMENTOSHENRY/5-6DPTOSHENRY.webp',],
+        cliente: 'WALDEN',
+        anio: '2023',
+        ubicacion: 'Tijuana, Baja California',
+        superficie: '10,312.00 m²',
+        descripcion: 'Diseño, cálculo y modelado BIM de las instalaciones: hidráulica, sanitaria y pluvial para torre de departamentos.'
+    },
+    16: {
+        imagenes: [
+            '/Website/Resources/Portfolio/16NAVEINDSUTRIALPRISMAVIII/1-2IHPRISMAVIII.webp',
+            '/Website/Resources/Portfolio/16NAVEINDSUTRIALPRISMAVIII/2-2ISPRISMAVIII.webp'],
+        cliente: 'ATISA',
+        anio: '2023',
+        ubicacion: 'Tijuana, Baja California',
+        superficie: '8984.20 m²',
+        descripcion: 'Diseño y cálculo de las instalaciones: hidráulica y sanitaria.'
+    },
+    17: {
+        imagenes: [
+            '/Website/Resources/Portfolio/17NAVEINDUSTRIALPRISMAIX/1-6IPPRISMAIX.webp',
+            '/Website/Resources/Portfolio/17NAVEINDUSTRIALPRISMAIX/3-6PRISMAIX.webp',
+            '/Website/Resources/Portfolio/17NAVEINDUSTRIALPRISMAIX/2-6PRISMAIX.webp',
+            '/Website/Resources/Portfolio/17NAVEINDUSTRIALPRISMAIX/4-6PRISMAIX.webp',
+            '/Website/Resources/Portfolio/17NAVEINDUSTRIALPRISMAIX/5-6PRISMAIX.webp',
+            '/Website/Resources/Portfolio/17NAVEINDUSTRIALPRISMAIX/6-6PRISMAIX.webp'],
+        cliente: 'ATISA',
+        anio: '2024 - Presente',
+        ubicacion: 'Tijuana, Baja California',
+        superficie: '9286.00 m²',
+        descripcion: 'Diseño y construcción de sistema pluvial.'
+    },
+    18: {
+        imagenes: [
+            '/Website/Resources/Portfolio/18NAVEINDUSTRIALPRISMAX/1-3IHPRISMAX.webp',
+            '/Website/Resources/Portfolio/18NAVEINDUSTRIALPRISMAX/2-3ISPRISMAX.webp',
+            '/Website/Resources/Portfolio/18NAVEINDUSTRIALPRISMAX/3-3IPPRISMAX.webp'],
+        cliente: 'ATISA',
+        anio: '2023',
+        ubicacion: 'Tijuana, Baja California',
+        superficie: '8095.50 m²',
+        descripcion: 'Diseño y cálculo de las instalaciones: hidráulica, sanitaria y pluvial.'
     }
 };
+
 
 $(document).ready(function(){
     $('.ver-detalles').on('click', function(){
